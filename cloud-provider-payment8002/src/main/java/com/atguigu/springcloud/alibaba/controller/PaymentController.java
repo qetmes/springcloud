2b6aeb692390060c94760b680f7d89e229d63c71
qetmes <55390882+qetmes@users.noreply.github.com>
2021-04-27 17:31:35 +0800

    Add files via upload

---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/alibaba/controller/PaymentController.java
@@ -0,0 +1,71 @@
+package com.atguigu.springcloud.alibaba.controller;
+
+import com.atguigu.springcloud.alibaba.entities.CommonResult;
+import com.atguigu.springcloud.alibaba.entities.Payment;
+import com.atguigu.springcloud.alibaba.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    //把yml配置文件中的server.port属性值取出来
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(@RequestBody Payment payment){
+        int result = paymentService.create(payment);
+        log.info("*****插入结果："+result);
+        if (result>0){  //成功
+            return new CommonResult(200,"插入数据库成功,serverPort:"+serverPort,result);
+        }else {
+            return new CommonResult(444,"插入数据库失败",null);
+        }
+    }
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("*****查询结果："+payment);
+        if (payment!=null){  //说明有数据，能查询成功
+            return new CommonResult(200,"查询成功，serverPort:" + serverPort,payment);
+        }else {
+            return new CommonResult(444,"没有对应记录，查询ID："+id,null);
+        }
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for(String element:services){
+            log.info("........element:" + element);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for(ServiceInstance instance : instances){
+            log.info(instance.getServiceId()+"\t" + instance.getHost() + "\t" +
+                    instance.getPort() + "\t" + instance.getUri());
+        }
+
+        return this.discoveryClient;
+    }
+
+    @GetMapping(value = "/payment/lb")
+    public String getPaymentLB(){
+        return serverPort;
+    }
+}
+
